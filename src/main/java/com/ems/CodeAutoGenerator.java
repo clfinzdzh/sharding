cc6cb272290edce1c7c6919491477ced4ea411f4
caolifeng <clfforjob@126.com>
2019-08-19 18:04:43 +0800

    ""

--- src/main/java/com/ems/CodeAutoGenerator.java
+++ src/main/java/com/ems/CodeAutoGenerator.java
@@ -1,14 +1,17 @@
 package com.ems;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -78,13 +81,10 @@ public class CodeAutoGenerator {
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setUrl("jdbc:mysql://192.168.200.51:3306/ems?useUnicode=true&useSSL=false&characterEncoding=utf8&allowPublicKeyRetrieval=true&serverTimezone=GMT");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
-        //dsc.setUsername("root");
-        //dsc.setPassword("1qaz2wsx");
         dsc.setUsername("yjanquan");
         dsc.setPassword("yjanquan");
         return dsc;
     }
-
 
 
     /**
@@ -93,7 +93,7 @@ public class CodeAutoGenerator {
     public static PackageConfig crearePackageConfig() {
         PackageConfig pc = new PackageConfig();
         pc.setModuleName(moduleName);
-        pc.setParent("com.ems");
+        pc.setParent("com.etcc");
         pc.setController("controller");
         pc.setMapper("mapper");
         pc.setEntity("entity.domain");
@@ -115,28 +115,41 @@ public class CodeAutoGenerator {
         focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
-                return projectPath + "/com/ems/" + moduleName + "/mapper/xml/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+                return projectPath + "/com/etcc/" + moduleName + "/mapper/xml/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
 
         focList.add(new FileOutConfig("/templates/adapter.java.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
-                return projectPath + "/com/ems/" + moduleName + "/adapter/" + tableInfo.getEntityName() + "Adapter.java";
+                return projectPath + "/com/etcc/" + moduleName + "/adapter/" + tableInfo.getEntityName() + "Adapter.java";
             }
         });
 
         focList.add(new FileOutConfig("/templates/vo.java.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
-                return projectPath + "/com/ems/" + moduleName + "/entity/vo/" + tableInfo.getEntityName() + "Vo.java";
+                return projectPath + "/com/etcc/" + moduleName + "/entity/vo/" + tableInfo.getEntityName() + "Vo.java";
+            }
+        });
+
+        focList.add(new FileOutConfig("/templates/queryVo.java.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return projectPath + "/com/etcc/" + moduleName + "/entity/vo/" + tableInfo.getEntityName() + "QueryVo.java";
             }
         });
 
         focList.add(new FileOutConfig("/templates/dto.java.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
-                return projectPath + "/com/ems/" + moduleName + "/entity/dto/" + tableInfo.getEntityName() + "Dto.java";
+                return projectPath + "/com/etcc/" + moduleName + "/entity/dto/" + tableInfo.getEntityName() + "Dto.java";
+            }
+        });
+        focList.add(new FileOutConfig("/templates/queryDto.java.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return projectPath + "/com/etcc/" + moduleName + "/entity/dto/" + tableInfo.getEntityName() + "QueryDto.java";
             }
         });
         cfg.setFileOutConfigList(focList);
@@ -149,17 +162,18 @@ public class CodeAutoGenerator {
     public static StrategyConfig createStrategyConfig() {
         StrategyConfig strategy = new StrategyConfig();
         //设置表前缀
-        strategy.setTablePrefix("sys_","t_","o_","v_","d_");
+        strategy.setTablePrefix("sys_", "t_", "o_", "v_", "d_");
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
+        strategy.setTableFillList(tableFills());
+        strategy.setEntityTableFieldAnnotationEnable(true);
         String tableNames = scanner("表名");
         strategy.setInclude(tableNames.split("\\,"));
         strategy.setSuperEntityColumns(new String[]{"id"});
         strategy.setControllerMappingHyphenStyle(true);
-        strategy.setSuperEntityClass("com.ems.common.base.BaseEntity");
-        strategy.setSuperControllerClass("com.ems.common.base.PageRequest");
+        strategy.setSuperEntityClass("com.etcc.base.BaseEntity");
         return strategy;
     }
 
@@ -194,6 +208,15 @@ public class CodeAutoGenerator {
         mpg.setTemplate(createTemplateConfig());
         mpg.setPackageInfo(crearePackageConfig());
         mpg.execute();
+    }
+
+    private static List<TableFill> tableFills() {
+        List<TableFill> tableFields = Lists.newArrayList();
+        tableFields.add(new TableFill("created_by", FieldFill.INSERT));
+        tableFields.add(new TableFill("updated_by", FieldFill.INSERT_UPDATE));
+        tableFields.add(new TableFill("created_at", FieldFill.INSERT));
+        tableFields.add(new TableFill("updated_at", FieldFill.INSERT_UPDATE));
+        return tableFields;
     }
 
 }
